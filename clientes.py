b0fc43237edf4a529b9d5b4051b062f6b5b6e5ef
Ricardo Porto <ricardoporto91@gmail.com>
2025-10-13 11:21:40 +0100

    Adicionei as funções para adicionar, eliminar e listar clientes

--- clientes.py
+++ clientes.py
@@ -3,8 +3,7 @@ class Cliente:
     Classe que representa um cliente do hotel.
     """
 
-    def __init__(self, id_cliente, nome, email, telefone):
-        self.id_cliente = id_cliente
+    def __init__(self, nome, email, telefone):
         self.nome = nome
         self.email = email
         self.telefone = telefone
@@ -19,4 +18,35 @@ class Cliente:
 
     def __str__(self):
         """Retorna uma representação legível do cliente."""
-        return f"Cliente #{self.id_cliente}: {self.nome} ({self.email}, {self.telefone})"
+        return f"Cliente: {self.nome} ({self.email}, {self.telefone})"
+    
+clientes = [
+    Cliente("Ricardo ABCD", "email@email.pt", "919191919"),
+    Cliente("Daniel ABCD", "email1@email.pt", "911191919"),
+    Cliente("Andreia DCBA", "email21@email.pt", "911181919"),
+    Cliente("Manuela CBDA", "email13@email.pt", "911191919"),
+    Cliente("Zé DACB", "email221@email.pt", "911591919"),
+    Cliente("Joaquina ABCD", "email1@email.pt", "913791919"),
+    Cliente("Maria ABCD", "email112@email.pt", "914491919")
+]
+
+def adicionar_cliente(cliente: Cliente):
+    """Adiciona um cliente à lista de clientes"""
+
+    clientes.append(cliente)
+    return cliente
+
+def eliminar_cliente(cliente: Cliente):
+    """Elimina um cliente da lista de clientes"""
+
+    for c in clientes:
+        if c == cliente:
+            clientes.remove(c)
+            return f"Cliente {c} eliminado com sucesso"
+    
+
+def listar_clientes():
+    """Lista todos os clientes"""
+
+    for c in clientes:
+        print(c)
